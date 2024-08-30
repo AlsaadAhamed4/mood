6a77ade84572169cb34856660470db11ca8d4227
Alsaad Ahamed <alsaadahamed4@gmail.com>
2024-08-30 17:30:52 +0530

    - Checked auth and created a new user in db in not there and navigated to journals screen with a loading tsx

---
+++ app/new-user/loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div className="bg-indigo-500 ...">
+            <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+            </svg>
+            Loading...
+        </div>
+    )
+}
+
+export default Loading;
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,7 +1,28 @@
-const NewUser = () => {
-    return (
-        <div>This new User</div>
-    )
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+    const user = await currentUser();
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user?.id as string,
+        }
+    })
+    if (!match) {  // new user
+        const newUser = await prisma.user.create({
+            data: {
+                clerkId: user?.id as string,
+                email: user?.emailAddresses[0].emailAddress as string
+            }
+        })
+    }
+
+    redirect('/journal')
+}
+
+const NewUser = async () => {
+    await createNewUser();
 }
 
 export default NewUser;
