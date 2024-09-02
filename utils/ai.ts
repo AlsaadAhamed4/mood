f50a1d9e8e99e5b179811826be0135283224f93c
Alsaad Ahamed <alsaadahamed4@gmail.com>
2024-09-02 15:04:06 +0530

    Added formatting scripting with langChain open AI by using parsers and promptemplate which will help us to template the entry or a user prompt in well formatted and machine undestanding way.

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,3 +1,4 @@
+import { PromptTemplate } from 'langchain';
 import {OpenAI} from 'langchain/llms/openai';
 import {StructuredOutputParser} from 'langchain/output_parsers';
 import z from 'zod'; // for schema related things
@@ -6,15 +7,34 @@ const analysisSchema = z
   .object({
     mood : z.string().describe('the mood of the person who wrote the journal entry.'),
     summary : z.string().describe('Quick summary of the entire entry.'),
+    subject : z.string().describe('the subject of the journal entry'),
     negative : z.boolean().describe('is the journal entry negative? (i.e does it contain negative emotions?).'),
     color: z.boolean().describe('a hexadecimal color code the represents the mood of the entry. Example #0101fe for blue representing hapiness.'),
   });
 
 const parser =  StructuredOutputParser.fromZodSchema(analysisSchema)
 
-export const analyze = async (prompt)=>{
+const getPrompt = async (content) => {
+    const format_instructions = parser.getFormatInstructions() // we format the prompt in a json object etc etc
+  
+    const prompt = new PromptTemplate ({
+      template:
+        'Analyze the following journal entry. Follow the intrusctions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',  // we are saying that we need it this way
+      inputVariables: ['entry'], 
+      partialVariables: { format_instructions }, // same name as we have to define it.
+    })
+  
+    const input = await prompt.format({
+      entry: content,
+    })
+  
+    return input
+  }
+
+export const analyze = async (content)=>{
+    const input = await getPrompt(content); // formatting  and calling the open AI via zod  
     console.log('entered analyze');
-const model = new OpenAI({temperature : 0, modelName: 'gpt-3.5-turbo'}) // basically here we are referencing which model we are using of chat gpt, temparature describes the top opinion from the pool ( 0 means more real ) basically 
-const result = await model.call(prompt);
-console.log(result, 'this ai response');
+    const model = new OpenAI({temperature : 0, modelName: 'gpt-3.5-turbo'}) // basically here we are referencing which model we are using of chat gpt, temparature describes the top opinion from the pool ( 0 means more real ) basically 
+    const result = await model.call(input);
+    console.log(result, 'this ai response');
 }
