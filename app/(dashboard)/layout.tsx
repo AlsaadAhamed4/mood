4da1d25181fdb71e28bfdbdff7416e9ec79c105e
Alsaad Ahamed <alsaadahamed4@gmail.com>
2024-09-02 17:10:20 +0530

    Added navigation link for beter accessability

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,28 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function DashboardLayout({ children, }: { children: React.ReactNode }) {
+    const navData = [
+        { href: '/', label: 'Home' },
+        { href: '/journal', label: 'Journal' },
+    ]
     return (
         <div className="h-screen w-screen relative">
             <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-                Mood
+                <div>
+                    Mood
+                </div>
+                <ul>
+                    {
+                        navData.map((item) => (
+                            <li className="px-2 py-4 text-xl">
+                                <Link href={item.href} >
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
             </aside>
             <div className="ml-[200px] h-full">
                 <header className="h-[60px] border-b border-black/10">
